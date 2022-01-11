d29ef74091499cec8e159fde9a419ed7f64db330
Konstantin Stepanov <688stakan@gmail.com>
2022-01-11 11:09:53 +0300

    WeekEnds Task added

---
+++ WeekendsDays/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+// Если допустить, что дни недели пронумерованы с 1 до 7, тогда:
+//Console.WriteLine("Введите число от 1 до 7: ");
+//int number = int.Parse(Console.ReadLine());
+//if ( number > 5 )
+//    Console.WriteLine("Это выходной день");
+//else
+//    Console.WriteLine("Будний день");
+
+
+int number = new Random().Next(1, 8);
+Console.WriteLine("Если номер дня недели - " + number);
+if ( number > 5 )
+    Console.WriteLine("Это выходной день");
+else
+    Console.WriteLine("Будний день");
