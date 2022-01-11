ddf443765a21909b83fbb32f16bba0e8a8fca98c
Konstantin Stepanov <688stakan@gmail.com>
2022-01-11 11:37:13 +0300

    TwoSquareNumbers task added

---
+++ TwoSquareNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Введите число A - ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B - ");
+int numberB = int.Parse(Console.ReadLine());
+if ( numberA == numberB * numberB)
+    Console.WriteLine("Число А является квадратом числа B");
+else
+    if ( numberB == numberA * numberA)
+        Console.WriteLine("Число B является квадратом числа А");
+    else
+        Console.WriteLine("ни одно число не является квадратом другого");
